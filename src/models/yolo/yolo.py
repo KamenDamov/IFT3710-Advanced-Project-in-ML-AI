d2f0541a2b03fe97f9d663f1d77b604b9159a8b6
ggenois <guillaumegenois@yahoo.ca>
2025-03-30 12:10:11 -0400

    test training with given dataset coco128.yaml

--- src/models/yolo/yolo.py
+++ src/models/yolo/yolo.py
@@ -5,7 +5,7 @@ model = YOLO("yolo12n.pt")  # load an official model
 # model = YOLO("path/to/best.pt")  # load a custom model
 # model = YOLO("yolo11n-seg.pt")
 
-results = model.train(data="/home/ggenois/PycharmProjects/IFT3710-Advanced-Project-in-ML-AI/dataset/data.yaml", epochs=100)
+results = model.train(data="coco128.yaml", epochs=5)
 
 
 # Validate the model
@@ -13,21 +13,21 @@ metrics = model.val()
 print("Mean Average Precision for boxes:", metrics.box.map)
 print("Mean Average Precision for masks:", metrics.seg.map)
 
-# Predict with the model
-results = model(["/home/ggenois/PycharmProjects/IFT3710-Advanced-Project-in-ML-AI/data/cell_00225.png", "/home/ggenois/PycharmProjects/IFT3710-Advanced-Project-in-ML-AI/data/cell_00849.png"])  # predict on an image
-
-# Access the results
-i = 0
-for result in results:
-#    xy = result.masks.xy  # mask in polygon format
-#    xyn = result.masks.xyn  # normalized
-#    masks = result.masks.data  # mask in matrix format (num_objects x H x W)
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
-    result.show()  # display to screen
-    result.save(filename=f"data/result_{i}.jpg")  # save to disk
-    i += 1
+# # Predict with the model
+# results = model(["/home/ggenois/PycharmProjects/IFT3710-Advanced-Project-in-ML-AI/data/cell_00225.png", "/home/ggenois/PycharmProjects/IFT3710-Advanced-Project-in-ML-AI/data/cell_00849.png"])  # predict on an image
+#
+# # Access the results
+# i = 0
+# for result in results:
+# #    xy = result.masks.xy  # mask in polygon format
+# #    xyn = result.masks.xyn  # normalized
+# #    masks = result.masks.data  # mask in matrix format (num_objects x H x W)
+#     boxes = result.boxes  # Boxes object for bounding box outputs
+#     masks = result.masks  # Masks object for segmentation masks outputs
+#     keypoints = result.keypoints  # Keypoints object for pose outputs
+#     probs = result.probs  # Probs object for classification outputs
+#     obb = result.obb  # Oriented boxes object for OBB outputs
+#     result.show()  # display to screen
+#     result.save(filename=f"data/result_{i}.jpg")  # save to disk
+#     i += 1
 
