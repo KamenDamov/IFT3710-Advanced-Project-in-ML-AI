65c198e0c9c6c117e0e5a84b31dc9f3ee1ad3aca
ggenois <guillaumegenois@yahoo.ca>
2025-04-02 21:44:57 -0400

    labels au lieu de masks

--- src/data_preprocess/unify/convert_CellPose.py
+++ src/data_preprocess/unify/convert_CellPose.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 def main(root, destination):
     # Define paths
     image_output_dir = os.path.join(destination, "images")
-    mask_output_dir = os.path.join(destination, "masks")
+    mask_output_dir = os.path.join(destination, "labels")
     os.makedirs(destination, exist_ok=True)
     os.makedirs(image_output_dir, exist_ok=True)
     os.makedirs(mask_output_dir, exist_ok=True)
