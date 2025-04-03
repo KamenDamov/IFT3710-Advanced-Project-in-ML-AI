64305401f81a638e7e93ac8ae5280a49f503b5db
ggenois <guillaumegenois@yahoo.ca>
2025-04-02 21:32:31 -0400

    ajout makedirs de destination

--- src/data_preprocess/unify/convert_CellPose.py
+++ src/data_preprocess/unify/convert_CellPose.py
@@ -10,6 +10,7 @@ def main(root, destination):
     # Define paths
     image_output_dir = os.path.join(destination, "images")
     mask_output_dir = os.path.join(destination, "masks")
+    os.makedirs(destination, exist_ok=True)
     os.makedirs(image_output_dir, exist_ok=True)
     os.makedirs(mask_output_dir, exist_ok=True)
 
